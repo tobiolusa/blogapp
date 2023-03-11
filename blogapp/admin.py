ff5966a302205ea4f9c0c6e4fbb158815309d390
tobiolusa <olusamiracle@gmail.com>
2023-03-11 01:35:44 +0100

    Comment feature added

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
@@ -7,5 +7,12 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'status','created_on')
+    list_filter = ("active",)
+    search_fields = ['post', 'name']
+    prepopulated_fields = {'post': ('body',)}
+    
     
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views import generic
+from .form import CommentForm
 
 class PostListView(generic.ListView):
     model = Post
@@ -10,3 +11,21 @@ class PostListView(generic.ListView):
 class PostDetailView(generic.DetailView):
     model = Post
     template_name = 'post-detail.html'
+    def comment_here(request, slug):
+        comments = Post.comments.filter(active=True)
+        new_comment = None
+        if request.method == 'POST':
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                new_comment = comment_form.save(commit=False)
+                new_comment.post = Post
+                new_comment.save()
+                
+        else:
+            comment_form = CommentForm()
+        
+        return render(request, {'post': Post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
+        
